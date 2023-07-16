de2e32929e78cb50d78048c2ec92756b04f5f69d
kitkatyourmom <sarahlridings@gmail.com>
2023-07-16 15:33:23 -0600

    start model index, define colums in Product.js

--- models/index.js
+++ models/index.js
@@ -6,11 +6,33 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo( Category, {
+  foreignKey: '',
+});
+
 // Categories have many Products
+
+Category.belongsToMany(Product, {
+  foreignKey: '',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany( Tag, {
+  through: {
+    model: ProductTag
+  },
+  as ''
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany( Product, {
+  through: {
+    model: ProductTag
+  },
+  as ''
+});
 
 module.exports = {
   Product,
